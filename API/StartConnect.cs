bbb8ecb3ae6fc61135fa0fd98de3850c15b4d9b4
David Lee <dcl@agence-sesame.fr>
2022-05-15 10:58:15 +0200

    Add files via upload

---
+++ API/StartConnect.cs
@@ -0,0 +1,21 @@
+﻿using DNNrocketAPI.Components;
+using RocketBlog.Components;
+using Simplisity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RocketBlog.API
+{
+    public partial class StartConnect : DNNrocketAPI.APInterface
+    {
+        public override Dictionary<string, object> ProcessCommand(string paramCmd, SimplisityInfo systemInfo, SimplisityInfo interfaceInfo, SimplisityInfo postInfo, SimplisityInfo paramInfo, string langRequired = "")
+        {
+            paramCmd = paramCmd.Replace("rocketblog_", "rocketcatalog_");
+            systemInfo.SetXmlProperty("genxml/systemkey", "rocketblog");
+            var catalogStartConnect = new RocketCatalog.API.StartConnect();
+            return catalogStartConnect.ProcessCommand(paramCmd, systemInfo, interfaceInfo, postInfo, paramInfo, langRequired);
+        }
+    }
+
+}
---
+++ Render/RocketBlogTokens.cs
@@ -0,0 +1,16 @@
+﻿using DNNrocketAPI.Components;
+using RazorEngine.Text;
+using Simplisity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RocketBlog.Components
+{
+    public class RocketBlogTokens<T> : DNNrocketAPI.render.DNNrocketTokens<T>
+    {
+
+
+    }
+}
